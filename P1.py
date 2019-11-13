2649f428c2e35ba69663c751094166d8ef324f33
Nissssssssssssy <52127165+Nissssssssssssy@users.noreply.github.com>
2019-11-13 17:40:34 +0900

    Add files via upload

---
+++ P1.py
@@ -0,0 +1,22 @@
+mtg = "Hello World"
+print(mtg)
+
+listed = ["a","b","c"]
+for list in listed:
+    print(list)
+
+x = 8
+if x >= 5:
+    print(True)
+else:
+    print(False)
+
+# 基本的にはprint()で出力
+#変数はなにかをいれておく箱のようなもので、最後に定義したものがその変数の値になる
+
+fruits_dict = {"apple": 100, "orange": 150}
+fruits_dict["apple"] = 200
+print(fruits_dict)
+
+print(7.7/2)
+print(7//2,7%2)
